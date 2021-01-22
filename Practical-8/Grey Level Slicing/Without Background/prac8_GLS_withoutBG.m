499da29cd7f9a8de584d288b0ab4bdda12fbb276
Anthony Thomas <54772502+AnthonyThomas20@users.noreply.github.com>
2021-01-22 20:19:43 +0530

    Create prac8_GLS_withoutBG.m

---
+++ Practical-8/Grey Level Slicing/Without Background/prac8_GLS_withoutBG.m
@@ -0,0 +1,13 @@
+I=imread('D:\Anthony\cameraman.jpg');
+Z=double(I);
+[row col]=size(Z);
+for i=1:1:row
+  for j=1:1:col
+    if((Z(i,j)>50)&&(Z(i,j)<150))
+      Z(i,j)=255;
+    else
+      Z(i,j)=0;
+    endif
+  endfor
+endfor
+imshow([I, uint8(Z)]);
